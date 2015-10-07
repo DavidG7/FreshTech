47baa69731db352d2ec7393ed072bca833d4be16
ricky <sil3nt884@hotmail.co.uk>
2015-10-07 16:25:00 +0100

    update test class

--- src/main/java/com/netbuilder/entities/CustomerOrder.java
+++ src/main/java/com/netbuilder/entities/CustomerOrder.java
@@ -6,69 +6,77 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.PrimaryKeyJoinColumn;
 
+/**
+ * @author Ricky Luu Holds customers Orders details
+ */
 
 @Entity
 public class CustomerOrder {
-	/**
-	 * @author Ricky Luu
-	 * Holds customers Orders details 
-	 */
-	
+
 	@Id
-	@GeneratedValue (
-			strategy = GenerationType.IDENTITY
-	)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int customerOrderID;
-	@OneToMany
-	@PrimaryKeyJoinColumn (
-			name="customerID"
-			
+	@ManyToOne
+	@PrimaryKeyJoinColumn(name = "customerID"
+
 	)
-	private Customer customer;
-	private Address address;
-	private Date dataOfOrder;
+	private int  customerID;
+	private Date dateOfOrder;
 	private String customerOrderStatus;
+	private int orderTotal;
 	private String deliveryAddress;
-	// Product address;
 	
-	
-	public CustomerOrder(){}
-	
+
+	public CustomerOrder() {
+	}
+
 	/**
-	 * Create a new CustomerOrder 
+	 * Create a new CustomerOrder
+	 * 
 	 * @param customerOrderID
 	 * @param customerID
 	 * @param dataOfOrder
 	 * @param customerOrderStatus
 	 * @param deliveryAddress
 	 */
-	
-	public CustomerOrder(int customerOrderID,Date dataOfOrder, String customerOrderStatus, String deliveryAddress ){
-		this.customerOrderID=customerOrderID;
-		this.dataOfOrder=dataOfOrder;
-		this.customerOrderStatus=customerOrderStatus;
-		this.deliveryAddress=deliveryAddress;
+
+	public CustomerOrder(int customerOrderID, int customerID, Date dataOfOrder,
+			String customerOrderStatus, String deliveryAddress) {
+		this.customerOrderID = customerOrderID;
+		this.customerID = customerID;
+		this.dateOfOrder = dataOfOrder;
+		this.customerOrderStatus = customerOrderStatus;
+		this.deliveryAddress = deliveryAddress;
+		
+		
 	}
-	
+
 	/**
-	 * Updates Customer Orders 
+	 * Updates Customer Orders
 	 */
-	public void updateCustomerOrder(){}
-	
+	public void updateCustomerOrder() {}
 	/**
-	 * Displays Orders 
+	 * Displays Orders
 	 */
+	public String  viewCustomerOrder() {
+		return  "OrderID:" +customerOrderID + "\nCustomerID:"+customerID+"\nCustomer Order Status:"+customerOrderStatus+ "\nDelivery Address:"+deliveryAddress;
+		}
 	
-	public void  viewCustomerOrder(){}
-	public Date getDataOfOrder() {return dataOfOrder;}
-	public void setDataOfOrder(Date dataOfOrder) {this.dataOfOrder = dataOfOrder;}
+	public Date getDateOfOrder() {return dateOfOrder;}
+	public void setDateOfOrder(Date dateOfOrder) {this.dateOfOrder = dateOfOrder;}
 	public int getCustomerOrderID() {return customerOrderID;}
 	public void setCustomerOrderID(int customerOrderID) {this.customerOrderID = customerOrderID;}
 	public String getCustomerOrderStatus() {return customerOrderStatus;}
 	public void setCustomerOrderStatus(String customerOrderStatus) {this.customerOrderStatus = customerOrderStatus;}
 	public String getDeliveryAddress() {return deliveryAddress;}
 	public void setDeliveryAddress(String deliveryAddress) {this.deliveryAddress = deliveryAddress;}
+	public int getCustomerID () {return customerID;}
+	public void setCustomerID(int customerID) {this.customerID = customerID;}
+	public int getOrderTotal() {return orderTotal;}
+	public void setOrderTotal(int orderTotal) {this.orderTotal = orderTotal;}
+	
+	
 }
