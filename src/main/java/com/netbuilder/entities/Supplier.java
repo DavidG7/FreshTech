0d74001bf0903a76240aea343725f1261727640e
nbcchris <chris.poole@netbuilder.com>
2015-10-07 10:23:50 +0100

    Adding updated Supplier and SupplierTest Classes

--- src/main/java/com/netbuilder/entities/Supplier.java
+++ src/main/java/com/netbuilder/entities/Supplier.java
@@ -8,9 +8,31 @@ public class Supplier {
 	private int phone;
 	private String preferredContactMethod;
 	
+	/**-------------------------------------------------------------------------------------------------------------------------------------
+	 * Supplier:
+	 * -------------------------------------------------------------------------------------------------------------------------------------
+	 * Supplier can be Constructor using one of two constructors:
+	 * 		Supplier(int id, String name) creates a Supplier with a non-null supplierId and supplierName
+	 * 		Supplier(int id, String name, String email, int phone, String pref) creates a fully instantiated Supplier
+	 *		
+	 * 		Supplier contains no methods besides getters and setters.
+	 * 
+	 * 		NOTE: preferredContactMethod could potentially be better as an Enum but with such few states its even possible to use a Boolean.
+	 * 			  Currently a String is being used with either the value "Phone" or "Email"
+	 * -------------------------------------------------------------------------------------------------------------------------------------
+	 */
+	
 	public Supplier (int id, String name){
 		this.supplierId = id;
 		this.supplierName = name;
+	}
+	
+	public Supplier (int id, String name, String email, int phone, String pref){
+		this.supplierId = id;
+		this.supplierName = name;
+		this.email = email;
+		this.phone = phone;
+		this.preferredContactMethod = pref;
 	}
 	
 	public void setId(int x){	supplierId = x;	}
