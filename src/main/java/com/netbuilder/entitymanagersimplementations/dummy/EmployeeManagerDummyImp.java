139aec5655e323b465851bf329fe4cc1d9b4dea4
unknown <dgordon@PC-CZC5300433.netbuilder.private>
2015-10-07 14:58:00 +0100

    EmployeeManager Update

---
+++ src/main/java/com/netbuilder/entitymanagersimplementations/dummy/EmployeeManagerDummyImp.java
@@ -0,0 +1,46 @@
+package com.netbuilder.entitymanagersimplementations.dummy;
+
+import java.util.ArrayList;
+
+import com.netbuilder.entities.Employee;
+import com.netbuilder.entitymanagers.EmployeeManager;
+
+public class EmployeeManagerDummyImp implements EmployeeManager{
+
+	@Override
+	public void persistEmployee(Employee employee) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void persistEmployees(ArrayList<Employee> employees) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Employee findByName(String employeeName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Employee> getEmployees() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateEmployee(Employee employee) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeEmployee(Employee employee) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
