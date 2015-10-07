d121f7251d0315608966fa2be2b64df07fab96d5
nbcchris <chris.poole@netbuilder.com>
2015-10-07 09:37:16 +0100

    Supplier class added

--- src/main/java/com/netbuilder/entities/Supplier.java
+++ src/main/java/com/netbuilder/entities/Supplier.java
@@ -1,5 +1,31 @@
 package com.netbuilder.entities;
 
 public class Supplier {
-
+	
+	private int supplierId;
+	private String supplierName;
+	private String email;
+	private int phone;
+	private String preferredContactMethod;
+	
+	public Supplier (int id, String name){
+		this.supplierId = id;
+		this.supplierName = name;
+	}
+	
+	public void setId(int x){	supplierId = x;	}
+	public int getId(){	return supplierId;	}
+	
+	public void setName(String x){	supplierName = x;	}
+	public String getName(){	return supplierName;	}
+	
+	public void setEmail(String x){	email = x;	}
+	public String getEmail(){	return email;	}
+	
+	public void setPhone(int x){	phone = x;	}
+	public int getPhone(){	return phone;	}
+	
+	public void setPreferredContactMethod(String x){	preferredContactMethod = x;	}
+	public String getPreferredContactMethod(){	return preferredContactMethod;	}
+	
 }
