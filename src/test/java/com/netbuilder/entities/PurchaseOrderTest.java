8c8f8a95ff1a17a8fb3aa0e88e1767d10f57158f
unknown <vtomlinson@PC-CZC53003FG.netbuilder.private>
2015-10-07 17:27:54 +0100

    New Purchase Order Entity Test

--- src/test/java/com/netbuilder/entities/PurchaseOrderTest.java
+++ src/test/java/com/netbuilder/entities/PurchaseOrderTest.java
@@ -10,39 +10,49 @@ public class PurchaseOrderTest {
 	
 	@BeforeClass
 	public static void PurchaseOrderTest() {
-		tester = new PurchaseOrder (1, "Confirmed");
-		
+				
 	}
 	
 	
 	
 	@Test
 	public void testGetPurchaseOrderID() {
-		
+		tester = new PurchaseOrder (1, "Confirmed");
 		assertEquals (1, tester.getPurchaseOrderID());
 	
 	
 	}
 	
+	@Test
 	public void testGetPurchaseOrderStatus() {		
-		
+		tester = new PurchaseOrder (1, "Confirmed");
 		assertEquals ("Confirmed", tester.getPurchaseOrderStatus());
 	
 	}
 	
+	@Test
 	public void testSetPurchaseOrderID() {
-		
+		tester = new PurchaseOrder (1, "Confirmed");
 		tester.setPurchaseOrderID(11);
 		assertEquals (11, tester.getPurchaseOrderID());
 	
 	
 	}
 	
+	@Test
 	public void testSetPurchaseOrderStatus() {
-		
+		tester = new PurchaseOrder (1, "Confirmed");
 		tester.setPurchaseOrderStatus("Unconfirmed");
 		assertEquals ("Unconfirmed", tester.getPurchaseOrderStatus());
 	
+	}
+	
+	@Test
+	public void testUpdatePurchaseOrder() {
+		tester = new PurchaseOrder (1, "Confirmed");
+		tester.updatePurchaseOrder(5, "Unconfirmed");
+		assertEquals ("Unconfirmed", tester.getPurchaseOrderStatus());
+		assertEquals (5, tester.getPurchaseOrderID());
 	}
 	
 	
