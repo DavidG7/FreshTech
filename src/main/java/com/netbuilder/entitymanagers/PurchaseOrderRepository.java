72e9111a46162c407eb42683f3f47249c4302de0
unknown <vtomlinson@PC-CZC53003FG.netbuilder.private>
2015-10-08 11:43:02 +0100

    PurchaseOrderRepository

---
+++ src/main/java/com/netbuilder/entitymanagers/PurchaseOrderRepository.java
@@ -0,0 +1,30 @@
+package com.netbuilder.entitymanagers;
+
+import org.springframework.data.repository.CrudRepository;
+import com.netbuilder.entities.PurchaseOrder;
+import java.util.List;
+
+public interface PurchaseOrderRepository  extends CrudRepository<PurchaseOrder, Integer>{
+	
+	/**
+	 * This method will return a single method given by a specific Purchase Order ID
+	 * @param PurchaseOrderID 
+	 * @return
+	 * 
+	 */
+	
+	public PurchaseOrder findByPurchaseOrderID(int PurchaseOrderID);
+	
+	
+	/**
+	 * This method will return a list of statuses for the Purchase Order
+	 * @param PurchaseOrderStatus
+	 * @return
+	 */
+	
+	public List<PurchaseOrder> findByPurchaseOrderStatus(String PurchaseOrderStatus);
+	
+	
+
+
+}
