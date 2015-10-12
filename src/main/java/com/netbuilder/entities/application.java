40b6c2ba785ffc57f47932d5923bedd7ec953d7f
nbcchris <chris.poole@netbuilder.com>
2015-10-12 13:07:39 +0100

    Removed GUI Folder

--- src/main/java/com/netbuilder/entities/application.java
+++ src/main/java/com/netbuilder/entities/application.java
@@ -2,6 +2,9 @@ package com.netbuilder.entities;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class application  {
@@ -9,5 +12,11 @@ public class application  {
 	public static void main(String args []){
 		SpringApplication.run(application.class,args);
 	}
+	
+	@Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
+        return factory;
+    }
 
 }
