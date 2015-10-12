a4f18078f0626fda5bed5d6e352ae7a1a27f1493
nbcchris <chris.poole@netbuilder.com>
2015-10-12 10:40:51 +0100

    Renamed GUI to not clash with Application.class

--- src/main/java/com/netbuilder/GUI/IMS.java
+++ src/main/java/com/netbuilder/GUI/IMS.java
@@ -2,11 +2,12 @@ package com.netbuilder.GUI;
 
 import javax.swing.JFrame;
 
-public class Application extends JFrame {
+public class IMS extends JFrame {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-
+	
+	
 }
