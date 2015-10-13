5e26854ce87aa5014656c73932fe9e99495b4422
nbcchris <chris.poole@netbuilder.com>
2015-10-13 10:55:13 +0100

    Updated Server to run on localhost:7991

--- fire/src/main/java/com/netbuilder/contoller/JSPController.java
+++ fire/src/main/java/com/netbuilder/contoller/JSPController.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class JSPController {
 	
-	 @RequestMapping("/jsp")
+	 @RequestMapping("/")
 	    public String index(ModelAndView modelAndView) {
 	        return "index.jsp";
 	  }
