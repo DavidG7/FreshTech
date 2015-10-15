96237de6fef337bbb5f8d9741896a291744022a1
ham5051 <joshuaham@netbuilder.com>
2015-10-15 10:26:49 +0100

    finished about

--- fire/src/main/java/com/netbuilder/contoller/testController.java
+++ fire/src/main/java/com/netbuilder/contoller/testController.java
@@ -1,15 +1,29 @@
 package com.netbuilder.contoller;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class testController {
+public class TestController {
+    @Autowired
+	TestControllerConfig tcc;
 
+/*		@RequestMapping(value="/welcome")
+		public ModelAndView helloWorld() {
+	 
+			String message = "<br><div style='text-align:center;'>"
+					+ "<h3>********** Hello World, Spring MVC Tutorial</h3>This message is coming from CrunchifyHelloWorld.java **********</div><br><br>";
+			return new ModelAndView("welcome", "message", message);*/
+    
 	
-	 @RequestMapping("form")
-	    public String index(ModelAndView modelAndView) {
-	        return "form";
-	  }
+	 @RequestMapping(value="/paymentagreement")
+	 public String index(Model model) {
+		 	model.addAttribute("checkoutitem", tcc.orderItems().get(0).toString() );
+	        return "paymentagreement";
+	 }
 }
