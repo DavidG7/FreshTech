bea793d0753acead189ad2db71bec241a241944d
ricky <sil3nt884@hotmail.co.uk>
2015-10-15 17:11:44 +0100

    jsp

--- fire/src/main/java/com/netbuilder/contoller/testController.java
+++ fire/src/main/java/com/netbuilder/contoller/testController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class TestController {
+public class testController {
     @Autowired
 	TestControllerConfig tcc;
 
