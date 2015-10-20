067202f5d28bea4a650f490d6cf2e20ac843b586
nbcchris <chris.poole@netbuilder.com>
2015-10-20 16:32:11 +0100

    Removed errors from GUI

--- ims-desktop/src/main/java/GUIMain.java
+++ ims-desktop/src/main/java/GUIMain.java
@@ -35,7 +35,7 @@ public class GUIMain
 	    
 	    frame.add(new SplashScreen(frame));
 	    
-	    //DatabaseController dbc = new DatabaseController();
+	    DatabaseController dbc = new DatabaseController();
 	        
 	    //mongoClient = new MongoClient(mongohost, port);
 		//db = mongoClient.getDatabase("test"); 
@@ -51,15 +51,10 @@ public class GUIMain
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	    frame.setIconImage(icon);
-	    frame.setUndecorated(true);   
-	    frame.getRootPane().setWindowDecorationStyle(JRootPane.WARNING_DIALOG);   
-	        
-	    UIManager.put("InternalFrame.activeTitleBackground", new ColorUIResource(new Color(0,122,0) ));
-	    UIManager.put("InternalFrame.activeTitleForeground", new ColorUIResource(Color.WHITE));
-	    UIManager.put("InternalFrame.titleFont", new Font("Dialog", Font.PLAIN, 11));
+		   frame.setVisible(true);
+	  
 	    
-	    
+	
 	    
 	}
 }
