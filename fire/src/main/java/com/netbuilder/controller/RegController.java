39e8314885d9a90155a12a3e096e27083a438b49
ricky <sil3nt884@hotmail.co.uk>
2015-10-20 15:10:06 +0100

    reg page

--- fire/src/main/java/com/netbuilder/controller/RegController.java
+++ fire/src/main/java/com/netbuilder/controller/RegController.java
@@ -2,24 +2,52 @@ package com.netbuilder.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.netbuilder.entities.Customer;
 import com.netbuilder.entityrepositories.CustomerRepository;
 
+@Controller
 public class RegController {
 
 	
 	ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 	CustomerRepository CustomerRepositoryDummy = (CustomerRepository)context.getBean("CustomerRepositoryDummy");
-	List<Customer> custoemrs  = CustomerRepositoryDummy.findAll();
-	 
+	List<Customer> customers = CustomerRepositoryDummy.findAll();
+	
 	 @RequestMapping("Register")
-	    	public String landing(Model model) {
-		 		//model.addAttribute("discontinuedProducts", discontinuedProducts);
-	        	return "Register";
-	 	}
+	 public String   Register(Model model) {
+	        return "Register";
+	  }
+	 
+	@RequestMapping(value="postReg" ,method = RequestMethod.POST)
+	public String doPost (HttpServletRequest request, HttpServletResponse response) {
+			String name = request.getParameter("firstnamesignup");
+			String lname = request.getParameter("lastnamesignup");
+			String username = request.getParameter("usernamesignup");
+			String password = request.getParameter("passwordsignup");
+			String password2 = request.getParameter("passwordsignup_confirm");
+			String month = request.getParameter("month");
+			String day = request.getParameter("day");
+			String year = request.getParameter("year");
+			System.out.println(name);
+			System.out.println(lname);
+			System.out.println(username);
+			System.out.println(password);
+			System.out.println(password2);
+			System.out.println(month);
+			System.out.println(day);
+			System.out.println(year);
+			customers.add(new Customer());
+			return "redirect:/";	
+	}
 }
