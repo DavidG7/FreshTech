a0da12778506bdb4f4d769386dae57026388314a
vtomlinson <vincentt327@gmail.com>
2015-10-22 09:32:47 +0100

    JSP

--- fire/src/main/java/com/netbuilder/controller/JSPController.java
+++ fire/src/main/java/com/netbuilder/controller/JSPController.java
@@ -65,40 +65,16 @@ public class JSPController {
 	 public String  PaymentAgreement(ModelAndView modelAndView) {
 	        return "PaymentAgreement";
 	  }
-	 
-<<<<<<< HEAD
-	
-	 
-	 @RequestMapping("ProductCatalogue")
-	 public String   ProductCatalogue(ModelAndView modelAndView) {
-	        return "ProductCatalogue";
-	  }
-	 
-	 
-	 
-	
-=======
-	 @RequestMapping("Product")
-	 public String   Product(ModelAndView modelAndView) {
-	        return "Product";
-	  }
->>>>>>> f028feaac593a51487b1c181ecb6799d0bb09255
 
 	 @RequestMapping("UpdateAccount")
 	 public String UpdateAccount (ModelAndView modelAndView) {
 	        return "UpdateAccount";
 	  }
-<<<<<<< HEAD
-	
-	
-=======
-	 
+
 	 @RequestMapping("ViewCreditDetails")
 	 public String ViewCreditDetails (ModelAndView modelAndView) {
 	        return "ViewCreditDetails";
 	  }
-
-	 
 
 	 @RequestMapping("Landing")
 	 public String landing2(ModelAndView modelAndView) {
@@ -109,13 +85,7 @@ public class JSPController {
 	 public String temp(ModelAndView modelAndView) {
 	        return "Template";
 	  }
-	 
-	 @RequestMapping("ChooseAddress")
-	 public String ChooseAddress (ModelAndView modelAndView) {
-	        return "ChooseAddress";
-	  }
->>>>>>> f028feaac593a51487b1c181ecb6799d0bb09255
-	
+
 	 @RequestMapping("CustomerAccountPage")
 	 public String CustomerAccountPage (ModelAndView modelAndView) {
 	        return "CustomerAccountPage";
