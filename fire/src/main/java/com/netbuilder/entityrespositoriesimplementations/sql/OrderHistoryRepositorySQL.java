49438cefc1b8b3cbb8b55fe14f5435b33d3a544c
ham5051 <joshuaham@netbuilder.com>
2015-10-22 12:37:21 +0100

    orderHistoryREPOSITORY

--- fire/src/main/java/com/netbuilder/entityrespositoriesimplementations/sql/OrderHistoryRepositorySQL.java
+++ fire/src/main/java/com/netbuilder/entityrespositoriesimplementations/sql/OrderHistoryRepositorySQL.java
@@ -1,15 +1,23 @@
 package com.netbuilder.entityrespositoriesimplementations.sql;
 
 import java.sql.ResultSet;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
 
 import javax.sql.DataSource;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.ResultSetExtractor;
 
@@ -18,11 +26,11 @@ import com.netbuilder.SQLConfig;
 import com.netbuilder.entities.CustomerOrder;
 import com.netbuilder.entityrepositories.CustomerOrderRepository;
 
-public class OrderHistoryRepositorySQL  implements CustomerOrderRepository {
+public class OrderHistoryRepositorySQL implements CustomerOrderRepository {
 	ApplicationContext ctx = new AnnotationConfigApplicationContext(SQLConfig.class);
 	DataSource dataSource = (DataSource)ctx.getBean("dataSource");
     JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-    
+ 
 	@Override
 	public long count() {
 		// TODO Auto-generated method stub
@@ -53,16 +61,37 @@ public class OrderHistoryRepositorySQL  implements CustomerOrderRepository {
 		// TODO Auto-generated method stub
 		return false;
 	}
+	
+
 	@Override
 	public Iterable<CustomerOrder> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		jdbcTemplate = new JdbcTemplate(dataSource);
+		String sql = "SELECT orderid, customerid, orderdate, ordertotal, customerorderstatus, addressid FROM CustomerOrder";
+		
+		List<CustomerOrder> customerorders = new ArrayList<CustomerOrder>();
+		
+		List<Map<String, Object>> rows = jdbcTemplate.queryForList(sql);
+		
+		for (Map row : rows) {
+			CustomerOrder customerorder = new CustomerOrder();
+			customerorder.setCustomerOrderID(Integer.parseInt(String.valueOf(row.get("orderid"))));
+			customerorder.setCustomerID(Integer.parseInt(String.valueOf(row.get("customerid"))));
+			customerorder.setDateOfOrder((String)row.get("orderdate"));
+			customerorder.setOrderTotal(Integer.parseInt(String.valueOf(row.get("ordertotal"))));
+			customerorder.setCustomerOrderStatus((String)row.get("customerorderstatus"));
+			customerorder.setDeliveryAddress(Integer.parseInt(String.valueOf(row.get("addressid"))));
+		}
+		return customerorders;
 	}
+	
+	
+	
 	@Override
 	public Iterable<CustomerOrder> findAll(Iterable<Integer> arg0) {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
 	@Override
 	public CustomerOrder findOne(Integer arg0) {
 		// TODO Auto-generated method stub
@@ -77,19 +106,22 @@ public class OrderHistoryRepositorySQL  implements CustomerOrderRepository {
 	public <S extends CustomerOrder> Iterable<S> save(Iterable<S> arg0) {
 		// TODO Auto-generated method stub
 		return null;
-	}
-	@Override
-	public CustomerOrder findByCustomerOrderID(Integer OrderID) {
-		List<CustomerOrder> co= jdbcTemplate.queryForList("SELECT orderid, customerorderid, orderdate, ordertotal, customerorderstatus, addressid FROM CustomerOrder WHERE orderid =" + 5);
-		for(int i=0; i<co.size(); i++){
-			  System.out.println(co.get(i).getCustomerID());
-			  System.out.println(co.get(i).getCustomerOrderStatus());
-			  System.out.println(co.get(i).getDeliveryAddress());
-			  System.out.println(co.get(i).getOrderTotal());
 		}
+	
+	
+	@Override
+	public CustomerOrder findByCustomerID(Integer customerID) {
+		String sql = "SELECT orderid, customerorderid, orderdate, ordertotal, customerorderstatus, addressid FROM CustomerOrder WHERE customerid = " + customerID;
+		jdbcTemplate = new JdbcTemplate(dataSource);
 		
-		return null;
+		CustomerOrder customerorder = (CustomerOrder) jdbcTemplate.queryForObject(
+				sql, new Object[] { customerID }, new BeanPropertyRowMapper(CustomerOrder.class));
+		
+		return customerorder;
 	}
+	
+
+	
 	@Override
 	public ArrayList<CustomerOrder> findByDeliveryAddress(String deliveryAddress) {
 		// TODO Auto-generated method stub
