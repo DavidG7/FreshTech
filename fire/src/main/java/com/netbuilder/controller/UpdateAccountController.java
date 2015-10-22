e0462da1a479ca4de2bc05464b380029253f83b6
ricky <sil3nt884@hotmail.co.uk>
2015-10-22 16:45:35 +0100

    merge fixes

--- fire/src/main/java/com/netbuilder/controller/UpdateAccountController.java
+++ fire/src/main/java/com/netbuilder/controller/UpdateAccountController.java
@@ -1,14 +1,43 @@
 package com.netbuilder.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.netbuilder.entities.Customer;
+import com.netbuilder.entityrepositoriesimplementations.mongo.CustomerRepositoryMongo;
+
+
+ /**
+  * 
+  * @author rluu
+  *
+  */
 @Controller
 public class UpdateAccountController {
+	
+	CustomerRepositoryMongo CustomerRepositoryMongo = new CustomerRepositoryMongo();
 
 	 @RequestMapping("UpdateAccount")
-	 public String UpdateAccount (ModelAndView modelAndView) {
-	        return "UpdateAccount";
+	 public ModelAndView UpdateAccount (ModelAndView modelAndView, HttpSession session) {
+		 	ModelAndView view = new ModelAndView();
+		 	view.setViewName("UpdateAccount");
+		 	 String user =session.getAttribute("sessionUser")+"";
+			 if(user.equalsIgnoreCase("null")){
+				 view.setViewName("Register");
+			 }	
+			 else{
+				 Customer customer = CustomerRepositoryMongo.findByCustomerUsername(user);
+				 view.addObject("customer", customer);		
+			 } 
+	        return view;
 	  }
 	
+
+	 
+	 
 }
