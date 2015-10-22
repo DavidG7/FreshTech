f7d264c5b79e3496409572118136d65f4100b349
Dudderz <tom.dudley5@gmail.com>
2015-10-22 17:33:39 +0100

    end of day submission

--- fire/src/main/java/com/netbuilder/controller/PaymentAgreementController.java
+++ fire/src/main/java/com/netbuilder/controller/PaymentAgreementController.java
@@ -3,13 +3,41 @@ package com.netbuilder.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.netbuilder.entityrepositoriesimplementations.mongo.PaymentRepositoryMongo;
+
 @Controller
 public class PaymentAgreementController {
 
+	PaymentRepositoryMongo PaymentRepositoryMongo = new PaymentRepositoryMongo();
+		
 	 @RequestMapping("PaymentAgreement")
-	 public String  PaymentAgreement(ModelAndView modelAndView) {
-	        return "PaymentAgreement";
-	  }
+	 public String  PaymentAgreement(ModelAndView modelAndView) 
+	 {
+		 for(int i = 0; i < PaymentRepositoryMongo.findAll().size(); i++)
+		 {
+			 System.out.println("");
+			 System.out.println("Payment ID: " + PaymentRepositoryMongo.findAll().get(i).getPaymentID());
+			 System.out.println("Customer ID: " + PaymentRepositoryMongo.findAll().get(i).getCustomerID());
+			 System.out.println("Card Number: " + PaymentRepositoryMongo.findAll().get(i).getCardNumber());
+			 System.out.println("Sort Code: " + PaymentRepositoryMongo.findAll().get(i).getSortCode());
+
+		 }
+		 
+		 PaymentRepositoryMongo.delete(2);
+		 
+		 for(int i = 0; i < PaymentRepositoryMongo.findAll().size(); i++)
+		 {
+			 System.out.println("");
+			 System.out.println("Payment ID: " + PaymentRepositoryMongo.findAll().get(i).getPaymentID());
+			 System.out.println("Customer ID: " + PaymentRepositoryMongo.findAll().get(i).getCustomerID());
+			 System.out.println("Card Number: " + PaymentRepositoryMongo.findAll().get(i).getCardNumber());
+			 System.out.println("Sort Code: " + PaymentRepositoryMongo.findAll().get(i).getSortCode());
+
+		 }
+		 
+		 return "PaymentAgreement";
+	 }
 	
 	
 }
