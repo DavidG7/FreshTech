55f1d1820b00d6837ef520ae1f8be85b3edb88bb
Dudderz <tom.dudley5@gmail.com>
2015-10-27 16:43:29 +0000

    Helped fix a problem with chris

--- fire/src/main/java/com/netbuilder/controller/AboutController.java
+++ fire/src/main/java/com/netbuilder/controller/AboutController.java
@@ -72,13 +72,17 @@ public class AboutController {
 		 //Payment Methods
 		 paymentRepository.insert(new Payment (1, 978923, "76-26-46"));
 		 paymentRepository.insert(new Payment (2, 345325, "09-72-23"));
-		 paymentRepository.insert(new Payment (3, 234532, "10-24-83"));
+		 paymentRepository.insert(new Payment (3, 234532, "10-24-83"));*/
 		 
-		 /*Basket Lines
-		 basketRepository.insert(new Basket(productRepository.get[0]), 13, 1);
+		 //Basket Lines
+		 
+		 Basket bask = new Basket(productRepository.findByProductID(0), 13, 1);
+		 
+		 basketRepository.insert(bask);
 		 basketRepository.insert(new Basket(productRepository.get[3]), 6, 1);
 		 basketRepository.insert(new Basket(productRepository.get[2]), 1, 2);
-		 basketRepository.insert(new Basket(productRepository.get[1]), 3, 3);*/
+		 basketRepository.insert(new Basket(productRepository.get[1]), 3, 3);
+		 basketRepository.insert(new Basket(productRepository.get[0]), 13, 1);
 			return "About";
 	 }
 }
