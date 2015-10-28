112c9f1d7a329e7e8810a09f9932a91650dcc274
ricky <sil3nt884@hotmail.co.uk>
2015-10-28 10:27:51 +0000

    about controll commented

--- fire/src/main/java/com/netbuilder/controller/AddPaymentDetailsController.java
+++ fire/src/main/java/com/netbuilder/controller/AddPaymentDetailsController.java
@@ -3,6 +3,13 @@ package com.netbuilder.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+/**
+ * 
+ * @author rluu
+ *
+ */
+
+
 @Controller
 public class AddPaymentDetailsController {
 
