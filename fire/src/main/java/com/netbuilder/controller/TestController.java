9cf438ab21d5430b331665134e232e7cdb4b444b
ham5051 <joshuaham@netbuilder.com>
2015-10-28 15:04:52 +0000

    davids method

--- fire/src/main/java/com/netbuilder/controller/TestController.java
+++ fire/src/main/java/com/netbuilder/controller/TestController.java
@@ -5,13 +5,16 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 import com.netbuilder.DataConfig;
 import com.netbuilder.RepositoryConfig;
+import com.netbuilder.entities.Supplier;
+import com.netbuilder.entities.ProductSupplier;
+import com.netbuilder.entityrepositories.ProductSupplierRepository;
 import com.netbuilder.entityrepositories.SupplierRepository;
 
 public class TestController {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ApplicationContext context = new AnnotationConfigApplicationContext(DataConfig.class, RepositoryConfig.class);
+	/**	ApplicationContext context = new AnnotationConfigApplicationContext(DataConfig.class, RepositoryConfig.class);
 		SupplierRepository SupplierRepository = context.getBean(SupplierRepository.class);	
 				
 		System.out.println(SupplierRepository.toString());
@@ -23,7 +26,21 @@ public class TestController {
 		System.out.println(SupplierRepository.findBySupplierEmail("gnomebros@hotmail.com"));
 		System.out.println(SupplierRepository.findBySupplierName("Garden Stuff"));
 		System.out.println(SupplierRepository.findBySupplierPhone("77665539987"));
+		System.out.println(SupplierRepository.findOne(1));
 		
+		Supplier test = new Supplier(6,"Tom and Joshs big mad shop", "ballz", "012145644", "email");
+		System.out.println(test.);
+		SupplierRepository.save(test);
+		
+		System.out.println(SupplierRepository.findBySupplierPhone("77665539987"));
+		**/
+		ApplicationContext context1 = new AnnotationConfigApplicationContext(DataConfig.class, RepositoryConfig.class);
+		ProductSupplierRepository ProductSupplierRepository = context1.getBean(ProductSupplierRepository.class);	
+		
+		System.out.println(ProductSupplierRepository.findByProductID(2));
+		System.out.println(ProductSupplierRepository.findByPrice(2));
+		System.out.println(ProductSupplierRepository.exists(10));
+		System.out.println(ProductSupplierRepository.findOne(5));
+		System.out.println(ProductSupplierRepository.findBySupplierID(2));
 	}
-
 }
