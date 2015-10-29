cdea4fe47026a71bd63a561d78017be3e55df094
ricky <sil3nt884@hotmail.co.uk>
2015-10-29 16:01:14 +0000

    save method

--- fire/src/main/java/com/netbuilder/controller/OrderHistoryController.java
+++ fire/src/main/java/com/netbuilder/controller/OrderHistoryController.java
@@ -3,6 +3,7 @@ package com.netbuilder.controller;
  *  @author jham
  *  @author rluu
  */
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -18,10 +19,13 @@ import com.netbuilder.DataConfig;
 import com.netbuilder.RepositoryConfig;
 import com.netbuilder.entities.Customer;
 import com.netbuilder.entities.CustomerOrder;
+import com.netbuilder.entities.CustomerOrderLine;
+import com.netbuilder.entities.Product;
 import com.netbuilder.entityrepositoriesimplementations.mongo.ProductRepositoryMongo;
 import com.netbuilder.entityrepositories.CustomerOrderLineRepository;
 import com.netbuilder.entityrepositories.CustomerOrderRepository;
 import com.netbuilder.entityrepositories.CustomerRepository;
+import com.netbuilder.entityrepositories.ProductRepository;
 import com.netbuilder.entityrespositoriesimplementations.sql.OrderHistoryRepositorySQL;
 
 @Controller
@@ -31,6 +35,8 @@ public class OrderHistoryController {
 	CustomerRepository customerRepository = mongoContext.getBean(CustomerRepository.class);	
 	CustomerOrderRepository customerOrderRepository = mongoContext.getBean(CustomerOrderRepository.class);	
 	CustomerOrderLineRepository customerOrderLineRepository = mongoContext.getBean(CustomerOrderLineRepository.class);	
+	ProductRepository productRepository  = mongoContext.getBean(ProductRepository.class);	
+	
 	
 	 /*@RequestMapping("OrderHistory")
 	public String OrderHistory (){
@@ -92,11 +98,22 @@ public class OrderHistoryController {
 				}
 				else{
 				 	Customer customer = customerRepository.findByCustomerUsername(user);
-				 	
 				 	List<CustomerOrder> orders = customerOrderRepository.findByCustomerID(customer.getCustomerID());
-				 	
+				 	ArrayList<CustomerOrderLine>  customerOrderLine = new ArrayList<CustomerOrderLine>();
+				 	ArrayList<Product>  products = new ArrayList<Product>();
+				 	for(int i=0; i<orders.size(); i++){
+				 		CustomerOrderLine orderlines = customerOrderLineRepository.findByCustomerOrderLineID(orders.get(i).getOrderID());
+				 		System.out.println(orders.get(i).getOrderID());
+				 		customerOrderLine.add(orderlines);
+				 	}
+				 	for(int i=0; i<customerOrderLine.size(); i++){
+				 		products.add(productRepository.findByProductID(customerOrderLine.get(i).getProductID()));
+				 		System.out.println(products.get(i).getProductName());
+				 	}
+				 
 				 	view.setViewName("OrderHistory");
 				 	view.addObject("customerOrders", orders);
+				 	view.addObject("products", products);
 				 	return view;
 				}
 		return view;
